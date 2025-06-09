f749ecd5b433fec9f35b298805532b750c1edc3a
GordonZhang0320 <gordonz14@nycstudents.net>
2025-06-09 11:46:59 -0400

    Update Code.java

--- Code.java
+++ Code.java
@@ -3,9 +3,14 @@ public class Move{
 		Finch f = new Finch();
 		boolean corner=false;
 		int playerNum=1;
+		int players=4;
 	    
 		f.stopAll();
 		while(true) {
+			
+			if (players==1) {
+				f.print(playerNum +"Wins!");
+			}
 			
 		    while(!corner) {
 		    	f.setMotors(10, 10);
@@ -13,20 +18,26 @@ public class Move{
 		    		f.setMotors(0, 0);
 		    		corner=true;
 		    	}
+		    	printPlayer(f, playerNum);
 		    }
 		    
-		    f.playNote(50,2);
-		    f.setBeak(playerNum, playerNum, playerNum);
 		    
+		    if(f.getButton("B")) {
+	    		players--;
+	    	}
 		    while(corner) {
+		    	
+		    		
 		    	if (f.getButton("A")) {
 		    		playerNum++;
-		    		f.setMotors(90,0);
+		    		f.setTurn("R", 90 ,5);
 		    		corner=false;
 		    	}
+		    	printPlayer(f, playerNum);
+		    	
 		    }
 		    
-		    if (playerNum>4) {
+		    if (playerNum>players) {
 		    	playerNum=1;
 		    }
 		    
@@ -34,4 +45,20 @@ public class Move{
 	    
 	       
 	}
+	
+	public static void printPlayer(Finch f, int playerTurns) {
+    	if (playerTurns%4==1) {
+        	f.print("1");
+		    f.playNote(50,2);
+        } else if (playerTurns%4==2) {
+        	f.print("2");
+		    f.playNote(60,2);
+        } else if (playerTurns%4==3) {
+        	f.print("3");
+		    f.playNote(70,2);
+        } else {
+        	f.print("4");
+		    f.playNote(80,2);
+        }
+    }
 }
